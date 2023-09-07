6690fd715012ce06f002c632ea1eb3f64318377a
Mekha <mekhalnair@gmail.com>
2023-09-07 10:37:38 +0530

    task

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,80 +1,64 @@
-import React, { useState,useEffect } from "react";
+import React, { useState } from "react";
 import "../Login/Login.css";
-import {authentication} from "../../../firebase-config"
+import { authentication } from "../../../firebase-config";
+import { signInWithPopup, GoogleAuthProvider, FacebookAuthProvider, TwitterAuthProvider } from 'firebase/auth';
+import GoogleButton from 'react-google-button';
 
-import { signInWithPopup ,GoogleAuthProvider,FacebookAuthProvider,TwitterAuthProvider} from 'firebase/auth'
-import GoogleButton from 'react-google-button'
 const Login = () => {
+  const signGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(authentication, provider)
+      .then((re) => {
+        console.log(re);
+        setWindow(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Sign In Not Successful");
+      });
+  };
 
-  const signGoogle=()=>{
-    const provider=new GoogleAuthProvider();
-    signInWithPopup(authentication,provider)
-    .then((re)=>{
-        console.log(re)
-        setWindow(false)
-    })
-    .catch((err)=>{
-        console.log(err)
-        alert("Sign In Not Successful")
-    })
-}
+  const signTwitter = () => {
+    const provider = new TwitterAuthProvider();
+    signInWithPopup(authentication, provider)
+      .then((re) => {
+        console.log(re);
+        // setWindow(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Sign In Not Successful");
+      });
+  };
 
-const signTwitter=()=>{
-  const provider=new TwitterAuthProvider();
-  signInWithPopup(authentication,provider)
-  .then((re)=>{
-      console.log(re)
-      // setWindow(false)
-  })
-  .catch((err)=>{
-      console.log(err)
-      alert("Sign In Not Successful")
-  })
-}
+  const signFacebook = () => {
+    const provider = new FacebookAuthProvider();
+    signInWithPopup(authentication, provider)
+      .then((re) => {
+        console.log(re);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Sign In Not Successful");
+      });
+  };
 
-const [user, setUser] = useState(null)
-
-
-const signFacebook=()=>{
-  const provider=new FacebookAuthProvider();
-  signInWithPopup(authentication,provider)
-  .then((re)=>{
-    console.log(re)
-    
-})
-.catch((err)=>{
-    console.log(err)
-    alert("Sign In Not Successful")
-})
-
-}
   const [window, setWindow] = useState(true);
- 
+
   return (
     <>
-        {window == true ? (
-          <div  >
-            <h1 className="login--header">Login</h1>
-            
-   <GoogleButton
-  onClick={signGoogle}
-  
-/>
- 
-
-<button onClick={signFacebook}>Sign In with twitter</button>
-<button onClick={signTwitter}>Sign In with twitter</button>
-         
-          </div>
-        ) : (
-              <h1 className="thanks--vote">Thanks For voting</h1>
-            )}
-        
- 
-    
+      {window === true ? (
+        <div>
+          <h1 className="login--header">Login</h1>
+          <GoogleButton onClick={signGoogle} />
+          <button onClick={signFacebook}>Sign In with Facebook</button>
+          <button onClick={signTwitter}>Sign In with Twitter</button>
+        </div>
+      ) : (
+        <h1 className="thanks--vote">Thanks For Voting</h1>
+      )}
     </>
   );
 };
 
 export default Login;
- 
