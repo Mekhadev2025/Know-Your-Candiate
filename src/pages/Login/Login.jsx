b9af6809a30844a8fb4ded75dc5eaca91946d7f1
Mekha <mekhalnair@gmail.com>
2023-09-10 11:09:52 +0530

    tested google firestore

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,80 +3,68 @@ import "../Login/Login.css";
 import { authentication } from "../../../firebase-config";
 import { signInWithPopup, GoogleAuthProvider, FacebookAuthProvider, TwitterAuthProvider } from 'firebase/auth';
 import { FacebookLoginButton, GoogleLoginButton, TwitterLoginButton } from "react-social-login-buttons";
-import {collection,getDocs,addDoc} from "firebase/firestore"
-import {db} from "../../../firebase-config"
+import { collection, getDocs, addDoc } from "firebase/firestore"
+import { db } from "../../../firebase-config"
+
 const Login = (props) => {
-  const [users,setUsers]=useState([])
+  const [users, setUsers] = useState([]);
   const [value, setValue] = useState("");
   const [showWindow, setShowWindow] = useState(true);
 
-function getUsers(){
-  const collectionRef=collection(db,'users')
-   getDocs(collectionRef)
-   .then(response=>{
-  console.log(response.docs)
-   const Users =response.docs.map(doc=>({
-    data:doc.data(),
-    id:doc.id
-  }))
-    setUsers(Users)
-   })
+  function getUsers() {
+    const collectionRef = collection(db, 'users')
+    getDocs(collectionRef)
+      .then(response => {
+        console.log(response.docs)
+        const Users = response.docs.map(doc => ({
+          data: doc.data(),
+          id: doc.id
+        }))
+        setUsers(Users)
+      })
+      .catch(error => {
+        console.log(error.message)
+      })
+  }
 
+  useEffect(() => {
+    getUsers()
+    console.log("hellooooo")
+  }, [])
 
-   .catch(error=>{
-    console.log(error.message)
-   })
+  useEffect(() => {
+    console.log("Users=", users);
+  }, [users]);
 
-}
-   
+  const addUserToFirestore = (email) => {
+    const collectionRef = collection(db, 'users');
+    addDoc(collectionRef, { email })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error.message);
+      })
+  }
 
-useEffect(()=>{
-  getUsers()
-  console.log("hellooooo")
-},[])
+  const signGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(authentication, provider)
+      .then(re => {
+        console.log("response==", re); 
+        setShowWindow(false);
+        localStorage.setItem("email", re.user.email);
+        addUserToFirestore(re.user.email);
 
-
-useEffect(() => {
-  console.log("Users=",users);
-}, [users]);
-
-//adding doc to firestore
-const [email,setEmail]=useState("")
-function addUsers(){
-  const collectionRef=collection(db,'users')
-   addDoc(collectionRef,{email})
-   .then(response=>{
-  console.log(response)
-   }
-   )
-   .catch(error=>{
-    console.log(error.message)
-   })
-
-}
-   
-const signGoogle = () => {
-  const provider = new GoogleAuthProvider();
-  signInWithPopup(authentication, provider)
-    .then((re) => {
-      console.log("response==", re);
-      localStorage.setItem("email", re.user.email);
-      setEmail(re.user.email); // Update the email state
-      setValue(re.user.email); // Update the value state
-      setShowWindow(false);
-      console.log("Email", re.user.email);
-      props.voteInc();
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-};
-
-
-useEffect(() => {
-  console.log("new Email", email); // Use the email state here
-}, [email]);
-
+        // Update the value state
+       
+        console.log("Email", re.user.email);
+        props.voteInc();
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
 
 
 
